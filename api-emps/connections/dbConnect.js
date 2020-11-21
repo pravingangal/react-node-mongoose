34fcf53d8e44aa56f1bb466bbd83e1a902223dd0
Pravin Gangal <pravin.gangal@yahoo.com>
2020-11-21 21:04:09 +0530

    Add files via upload

---
+++ api-emps/connections/dbConnect.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+async function dbConnect() {
+
+
+      return  new Promise((resolve, reject) => {
+
+        mongoose.connect(process.env.DB_HOST_URL, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          useCreateIndex: true,
+        }).then((db)=> resolve(db))
+        .catch((err)=>reject(null))
+
+      });
+     
+
+}
+
+export default dbConnect;
