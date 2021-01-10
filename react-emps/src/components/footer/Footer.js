da59d31d7e39d506906ce637eda9bd51ab3b2688
Pravin Gangal <pravin.gangal@yahoo.com>
2021-01-10 17:22:46 +0530

    Update Footer.js

--- react-emps/src/components/footer/Footer.js
+++ react-emps/src/components/footer/Footer.js
@@ -11,8 +11,9 @@ const Footer = ({ empName }) => {
           <span>Home</span> &nbsp; &nbsp;<span>©2020 </span>&nbsp;&nbsp;
               <span>Corp &amp; Co.</span>
         </div>
-        <div className="footerBorder marginBottom10">&nbsp;</div>        
+        <div className="footerBorder marginBottom10">&nbsp;</div>             
       </div>
+      <div className="disFlex">Lorem Epsum Lorem Epsum Lorem Epsum</div>
     </footer>
   );
 };
