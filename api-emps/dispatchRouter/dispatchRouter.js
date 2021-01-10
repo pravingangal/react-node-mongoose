8028a3342a62d50278ff7ab85370e839ff646e27
Pravin Gangal <pravin.gangal@yahoo.com>
2021-01-10 17:50:59 +0530

    Update dispatchRouter.js

--- api-emps/dispatchRouter/dispatchRouter.js
+++ api-emps/dispatchRouter/dispatchRouter.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import empLoggedInRouter from "../routers/empLoggedInRouter";
+import empLoggedinSendEmailRouter  from "../routers/empLoggedinSendEmailRouter"
 
 
 const dispatchRouter = express.Router();
@@ -10,6 +11,9 @@ dispatchRouter.post("/*", (req, res) => {
   switch (req.path) {
     case "/empLoggedIn":
       empLoggedInRouter(req, res);
+      break;
+    case "/empLoggedInSendEmail":
+      empLoggedinSendEmailRouter(req, res);
       break;
 
     default:
