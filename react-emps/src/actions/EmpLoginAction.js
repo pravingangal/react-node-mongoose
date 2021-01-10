d966a5ff07efb80100eb931e9603671620a0a847
Pravin Gangal <pravin.gangal@yahoo.com>
2021-01-10 17:13:31 +0530

    Update EmpLoginAction.js

--- react-emps/src/actions/EmpLoginAction.js
+++ react-emps/src/actions/EmpLoginAction.js
@@ -2,6 +2,8 @@ import {
   EMP_LOGGED_IN,
   EMP_LOGGED_OUT,
   EMP_LOGGED_ERR,
+  EMP_LOGGED_IN_SEND_EMAIL,
+  EMP_LOGGED_SEND_EMAIL_ERR
 } from "../utilities/Types";
 import { EmpApi } from "../apiemps_serverops/EmpApi";
 
@@ -11,6 +13,14 @@ export const empLoggedIn = (emps) => {
   return {
     type: EMP_LOGGED_IN,    
     dataLoad: emps,
+  };
+};
+
+export const empLoggedInSendEmail = (empsEmails) => {
+
+  return {
+    type: EMP_LOGGED_IN_SEND_EMAIL,    
+    dataLoad: empsEmails,
   };
 };
 
@@ -24,12 +34,17 @@ export const empLoggedErr = (err) => {
   };
 };
 
+export const empLoggedInSendEmailErr = (err) => { 
+  return { type: EMP_LOGGED_SEND_EMAIL_ERR,   
+    errLoad: err 
+  };
+};
+
 export const empLoginAction = (empLoginData) => { 
   return (dispatch) => {
-    EmpApi.emps
-	
+    EmpApi.emps	
       .Login(empLoginData)
-      .then((res) => {        
+      .then((res) => {             
         localStorage.refreshJWToken = res.data.refreshToken;       
         dispatch(empLoggedIn(res.data));        
       })
@@ -41,6 +56,23 @@ export const empLoginAction = (empLoginData) => {
       });
   };
 };
+
+
+export const empLoginSendEmailAction = (empLoginSendEmailData) => { 
+  return (dispatch) => {
+    EmpApi.emps.loggedInSendEmail(empLoginSendEmailData).then((res) => {             
+        dispatch(empLoggedInSendEmail(res.data));        
+      }).catch((e) => {
+        if(e.response)
+        {         
+        dispatch(empLoggedInSendEmailErr(e.response.data));
+        }        
+      });
+  }
+  };
+
+
+
 
 export const empLogoutAction = () => (dispatch) => {
  
