28acf7d653dcf8cbb218b82436378b16922499ac
Pravin Gangal <pravin.gangal@yahoo.com>
2021-01-10 17:46:39 +0530

    Create empLoggedinSendEmailRouter.js

---
+++ api-emps/routers/empLoggedinSendEmailRouter.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+import nodemailer from "nodemailer";
+import empModel from "../empModel/empModel";
+import empMailer from "../empMailer/empMailer";
+
+
+
+function empLoggedinSendEmailRouter(req, res) {
+
+  const empLoginSendEmailData = req.body;
+  
+  
+  if(mongoose.connection.readyState===1)
+  {//*********************start mongoose.connection.readyState if************************ */
+
+      
+    empModel.countDocuments({},function (err,noOfDocs)
+    {//**********   start empModel.countDocuments ***************  //
+                            
+        if(noOfDocs>0)
+          {
+            
+            let empsSendEmail=[];
+                         
+              empMailer(empLoginSendEmailData)
+              .then((response) => {
+                             
+                res.status(200).send(response);         
+              })
+              .catch((err) => {                                        
+              });                        
+                                       
+          }
+          else
+          {                                       
+            res.status(400).json({
+              empsError: "No Records in database",
+            });
+          }
+
+    });//**********   end empModel.countDocuments ***************  //
+
+  }//******************end mongoose.connection.readyState if************************** */
+  else
+  {//*********************start mongoose.connection.readyState else************************ */
+    
+    res.status(400).json({
+      empsError: "Unable to Connect to Mongodb",
+    });
+  }//*********************end mongoose.connection.readyState else************************ */
+
+
+}
+
+export default empLoggedinSendEmailRouter;
