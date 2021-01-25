15e8c1a10ea84b0ed8ba603e7d7e5f52192ec827
Pravin Gangal <pravin.gangal@yahoo.com>
2021-01-25 17:51:22 +0530

    Update EmpsLoginEmailReducer.js

--- react-emps/src/reducers/EmpsLoginEmailReducer.js
+++ react-emps/src/reducers/EmpsLoginEmailReducer.js
@@ -1,40 +1,45 @@
 import {
   EMP_LOGGED_IN_SEND_EMAIL,
-  EMP_LOGGED_SEND_EMAIL_ERR
+  EMP_LOGGED_SEND_EMAIL_ERR,
+  EMP_LOGGED_OUT,
 } from "../utilities/Types";
-
-
 const initState = {  
   emailSentArr:[],
-  empEmailErrors:{}
+  empEmailErrors:null,
+  empEmailsLoading:false
 };
-
 const EmpsLoginEmailReducer = (state = initState, action = {}) => {
-  const newState = { ...state };
- 
-  switch (action.type) {
-    case   EMP_LOGGED_IN_SEND_EMAIL:
-      
+  const newState = { ...state }; 
+  switch (action.type) {   
+    case   EMP_LOGGED_IN_SEND_EMAIL:    
       if (action.dataLoad) {       
         return {
           ...state,                  
           emailSentArr:action.dataLoad,          
-          empEmailErrors:{}                  
+          empEmailErrors:null ,
+          empEmailsLoading:!state.empEmailsLoading              
         };
       }
       break;
-    case EMP_LOGGED_SEND_EMAIL_ERR:
-      if (action.dataLoad) {
+    case EMP_LOGGED_SEND_EMAIL_ERR:     
+      if (action.errLoad) {
       return {
         ...state,        
           emailSentArr:[],          
-          empEmailErrors:action.dataLoad.emps.empEmailErrors    
+          empEmailErrors:action.errLoad.empsError  ,
+          empEmailsLoading:!state.empEmailsLoading
       };
     }
     break;
+    case EMP_LOGGED_OUT:
+      return { 
+        ...state,         
+            emailSentArr:[],
+            empEmailErrors:null,
+            empEmailsLoading:!state.empEmailsLoading
+      }      
     default:
       return newState;
   }
 };
-
 export default EmpsLoginEmailReducer;
