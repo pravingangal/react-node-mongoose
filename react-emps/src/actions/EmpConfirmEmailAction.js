fad66d4f868c0699c7521c9bb2dd48f5cb1af9a1
Pravin Gangal <pravin.gangal@yahoo.com>
2021-01-25 18:10:35 +0530

    Add files via upload

---
+++ react-emps/src/actions/EmpConfirmEmailAction.js
@@ -0,0 +1,31 @@
+import {
+  EMP_EMAIL_CONFIRMED,
+  EMP_EMAIL_CONFIRMATION_ERR,    
+  } from "../utilities/Types";
+  import { EmpApi } from "../apiemps_serverops/EmpApi";
+    
+  export const empConfirmedEmail = (emps) => {  
+    return {
+      type:   EMP_EMAIL_CONFIRMED,    
+      dataLoad: emps,
+    };
+  };  
+  export const empConfirmedEmailErr = (err) => { 
+    return { type: EMP_EMAIL_CONFIRMATION_ERR,   
+      errLoad: err 
+    };
+  };  
+  export const empConfirmEmailAction = (empConfirmEmailData) => {      
+    return (dispatch) => {
+      const confirmationToken={confirmationToken:empConfirmEmailData};
+      EmpApi.emps.empConfirmedEmail(confirmationToken)
+       .then((res) => {                                      
+           dispatch(empConfirmedEmail(res.data));                
+         }).catch((e) => {          
+           if(e.response)
+           {         
+           dispatch(empConfirmedEmailErr(e.response.data));
+           }        
+         });
+    }
+    };
