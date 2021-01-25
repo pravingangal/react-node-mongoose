61bf6767442159dc592c46fd7c95bcb72175dd14
Pravin Gangal <pravin.gangal@yahoo.com>
2021-01-25 17:29:03 +0530

    Update empMailer.js

--- api-emps/empMailer/empMailer.js
+++ api-emps/empMailer/empMailer.js
@@ -4,10 +4,8 @@ import transportEmpEmail from "./transportEmpEmail";
 
 async function empMailer(empLoginSendEmailData) {
 
-    
-
   const emailArr= empLoginSendEmailData.map(async empValObj=>{
-    const response=await transportEmpEmail(empValObj,"Email");
+    const response=await transportEmpEmail(empValObj);
     return response;
   }
     );
