0d149ca906be2fd0269e76ec890decce444e5353
Pravin Gangal <pravin.gangal@yahoo.com>
2021-01-25 17:41:05 +0530

    Update ToolTip.js

--- react-emps/src/utilities/ToolTip.js
+++ react-emps/src/utilities/ToolTip.js
@@ -1,10 +1,7 @@
 import React from "react";
 import "./ToolTip.css"; 
 
-
 const EmpToolTip = ({empErrMsg,classList}) => { 
   return (<span  className={classList? classList + " empTooltip":" empTooltip"}>{" " +empErrMsg}</span>);
 }
-
 export default EmpToolTip;
- 
