23d4e8ea883e354d6b3138fe1b729a230ed7928a
Pravin Gangal <pravin.gangal@yahoo.com>
2021-05-03 18:07:48 +0530

    Add files via upload

---
+++ react-emps/src/routes/EmpNotLoggedinRoute.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+const EmpNotLoggedinRoute = ({ empValidated, component: Component, ...rest }) => {
+ return (    
+  <Route
+    {...rest}
+    render={(props) =>       
+      !empValidated ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/dashboard" />
+      )
+    }
+  />
+);
+}
+EmpNotLoggedinRoute.propTypes = {
+  component: PropTypes.object.isRequired,
+  empValidated: PropTypes.bool.isRequired,
+  
+};
+function mapStateToProps(state) {
+  return { empValidated: state.EmpsLoginReducer.empValidated,
+  };
+}
+
+export default connect(mapStateToProps)(EmpNotLoggedinRoute);
