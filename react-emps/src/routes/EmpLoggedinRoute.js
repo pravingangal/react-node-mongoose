3840d591efbc07292e80d2dfbddd9f1321878d02
Pravin Gangal <pravin.gangal@yahoo.com>
2021-05-03 18:07:14 +0530

    Create EmpLoggedinRoute.js

---
+++ react-emps/src/routes/EmpLoggedinRoute.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+const EmpLoggedinRoute = ({ empValidated, component: Component, ...rest }) => {
+ return (    
+  <Route
+    {...rest}
+    render={(props) =>       
+      empValidated ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/" />
+      )
+    }
+  />
+);
+}
+ EmpLoggedinRoute.propTypes = {
+  component: PropTypes.oneOfType([PropTypes.object.isRequired,PropTypes.func.isRequired]),
+  empValidated: PropTypes.bool.isRequired,
+};
+
+function mapStateToProps(state) {
+  return { empValidated: state.EmpsLoginReducer.empValidated,};
+}
+
+export default connect(mapStateToProps)(EmpLoggedinRoute);
