70f7d3da940ff9458a218f030ae6f6add2355e2b
Pravin Gangal <pravin.gangal@yahoo.com>
2021-05-03 18:05:53 +0530

    Create store.js

---
+++ react-emps/src/store/store.js
@@ -0,0 +1,19 @@
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import EmpsLoginReducer from "../reducers/EmpsLoginReducer";
+import EmpsEmailConfirmedReducer from "../reducers/EmpsEmailConfirmedReducer";
+
+const rootReducer = combineReducers({
+  EmpsLoginReducer,  
+  EmpsEmailConfirmedReducer
+});
+
+
+const store = createStore(  
+  rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))   
+  );
+  
+
+export default store;
