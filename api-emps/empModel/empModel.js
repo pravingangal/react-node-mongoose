ba67a1e0bdd608228215af4127f76f59b251f932
Pravin Gangal <pravin.gangal@yahoo.com>
2021-05-03 21:32:26 +0530

    Update empModel.js

--- api-emps/empModel/empModel.js
+++ api-emps/empModel/empModel.js
@@ -4,31 +4,35 @@ import uniqueValidator from "mongoose-unique-validator";
 import jwt from "jsonwebtoken";
 
 const empSchema = new mongoose.Schema(
-  {
-    id: { type: Number, required: true, indexes: true, unique: true },
-    name: { type: String, required: true },
+  {    
+    id: { type: Number, required: true, indexes: true, unique: true },    
+    name: { type: String, required: true },    
     mob: { type: String, required: true, unique: true },
     position: { type: String, required: true },
     dept: { type: String, required: true },
-    sal: { type: Number, required: true },
-    status: { type: String, required: true },
-    role: { type: String, required: true },
+    sal: { type: Number, required: true},
+    status: { type: String, required: true},
+    role: { type: String, required: true},    
     email: {
-      type: String,
+      type: Object,
       required: true,
       lowercase: true,
       indexes: true,
-      unique: true,
-    },
-    emailSent: { type: Boolean, default: false },
-    pwdHash: { type: String, required: true },
+      unique: true,      
+      error:{type:String,default:"None"},
+    },      
+    pwdHash: { type: String, required: true},    
+    confirmationToken: { type: String, dafault: "" }, 
+    emailSent: { type: Boolean, default: false}, 
     emailConfirmed: {
       type: Boolean,
-      default: false,
+      default: false,      
     },
-    confirmationToken: { type: String, dafault: "" },
-  },
-  { timestamps: true }
+    emailConfirmationErr:{type: String, dafault: null} ,
+    emailErr: { type: String, dafault: null},      
+    emailInfo: { type: String, dafault: null},
+  }, 
+  { timestamps: true ,retainKeyOrder:true},
 );
 
 empSchema.methods.encryptPassword = function encryptPassword(pwd) {
@@ -48,10 +52,9 @@ empSchema.plugin(uniqueValidator, {
   message: "Duplicate field error..Record not saved..",
 });
 
-
-empSchema.methods.getAdmin_JSON_WEB_TOKEN = function getAdmin_JSON_WEB_TOKEN(empID,emailSent,emailConfirmed,confirmationToken) {
+empSchema.methods.getAdmin_JSON_WEB_TOKEN = function getAdmin_JSON_WEB_TOKEN(empID,emailSent,emailConfirmed,confirmationToken,emailErr,emailInfo) {
   
-  return this._getAdmin_JSON_WEB_TOKEN(empID,emailSent,emailConfirmed,confirmationToken)
+  return this._getAdmin_JSON_WEB_TOKEN(empID,emailSent,emailConfirmed,confirmationToken,emailErr,emailInfo)
 };
 
 empSchema.methods.getEmp_JSON_WEB_TOKEN = function getEmp_JSON_WEB_TOKEN() {
@@ -78,10 +81,12 @@ empSchema.methods._getEmp_JSON_WEB_TOKEN = function empSchema() {
 };
 
 
-empSchema.methods._getAdmin_JSON_WEB_TOKEN = function empSchema(empID,emailSent,emailConfirmed,confirmationToken) {
-  
+empSchema.methods._getAdmin_JSON_WEB_TOKEN = function empSchema(empID,emailSent,emailConfirmed,confirmationToken,emailErr,emailInfo) {  
+                                                                                                                                       
   return jwt.sign(
     {
+      emailSent:emailSent,
+      emailConfirmed:emailConfirmed,
       id:empID,
       name: this.name,
       mob: this.mob,
@@ -90,11 +95,12 @@ empSchema.methods._getAdmin_JSON_WEB_TOKEN = function empSchema(empID,emailSent,
       sal: this.sal,
       status: this.status,
       role: this.role,
-      email: this.email,
-      emailSent:emailSent,
-      emailConfirmed:emailConfirmed,
+      email: this.email,     
       confirmationToken:confirmationToken,      
-      SUPER_ADMIN:false,  
+      SUPER_ADMIN:false,
+      emailConfirmationErr:this.emailConfirmationErr,
+      emailErr:emailErr,
+      emailInfo:emailInfo  
     },  
     process.env.JSONWT_SEC_KEY
   ); 
