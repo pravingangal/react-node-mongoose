8386bb7e20d999156884883abd51b04ea61b539d
Pravin Gangal <pravin.gangal@yahoo.com>
2021-05-03 18:47:32 +0530

    Add files via upload

---
+++ react-emps/src/apiemps_serverops/FetchData.js
@@ -0,0 +1,33 @@
+export  async function checkAPIServerConnection(url)  {
+  let data = null;
+  await fetch(url).then((response)=>{	  
+	                                    response.status===200?data=response:response.json();}).catch((err)=>{		  
+		                                });  
+  return  new Promise((resolve, reject) => {
+
+if(data)
+{  
+  resolve(data);
+}
+else
+{
+  reject({empsError:"Internal Server Error"})
+}
+  });
+};
+
+async function FetchData  (url)  {
+  const response = await fetch(url);
+  const data = await response.json();
+  return  new Promise((resolve, reject) => {
+  if(data)
+    {
+      resolve(data);
+    }
+    else
+    { 
+      reject({errs:"Error fetching data"})
+    }
+  });
+};
+export default FetchData;
