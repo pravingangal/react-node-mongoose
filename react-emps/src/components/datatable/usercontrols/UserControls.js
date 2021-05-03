aeb53e3a7cb4bd94b79445f726289f74494ec3b5
Pravin Gangal <pravin.gangal@yahoo.com>
2021-05-03 18:38:06 +0530

    Add files via upload

---
+++ react-emps/src/components/datatable/usercontrols/UserControls.js
@@ -0,0 +1,75 @@
+import React,{useState,useEffect} from "react";
+
+  export const InputControl= ({elemID,onChangeSel,selCheckedFlag})=>{
+   
+  const [isSelChecked,setIsSelChecked]=useState(false);
+ 
+  const onChange=(event)=>{           
+    setIsSelChecked(curr=>!curr);  
+    onChangeSel(event);
+    
+  }
+
+   useEffect(()=>{
+
+     if(selCheckedFlag)
+     {
+      setIsSelChecked(selCheckedFlag);      
+     }
+    
+   },[selCheckedFlag]
+   );
+
+        return (<input 
+          type="checkbox"
+          className={"custom-control-input"}  
+          id={elemID}           
+          checked={isSelChecked}
+          name={elemID} 
+          onChange={onChange}                                                                                                                          
+           />);     
+};
+
+export const LabelControl=({lblText,lblFor,classNames})=>{  
+
+                          return  (
+                                      <label 
+                                              htmlFor={lblFor} 
+                                              className={"custom-control-label " + classNames}
+                                      >
+                                      {lblText}
+                                      </label>
+                                  );           
+}
+
+
+export const IconControl=({iconId,iconText,classNames})=>{
+   
+  return (<i 
+            name={iconId} 
+            id={iconId} 
+            title={iconText} 
+            className={"fa fa-exclamation-triangle " + classNames}>              
+            </i>
+            );
+}
+
+export const ButtonControl=({btnText,classNames,onClick})=>{
+  return  (   <button  
+                  className={"btn btn-primary " + classNames} 
+                  onClick= {onClick}               
+              >
+                  {btnText}
+              </button>
+          );
+}
+
+export const LinkCtrl=({valText,classNames,onClick})=>{
+  return  (   <a 
+                  href={onClick} 
+                  className={"btn btn-primary"+classNames}                               
+              >
+                  {valText}
+              </a>
+          );
+}
