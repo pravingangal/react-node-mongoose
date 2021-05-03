b34d0e5c707f24e0a459e4b3873812a7196a3cc0
Pravin Gangal <pravin.gangal@yahoo.com>
2021-05-03 17:53:08 +0530

    Delete react-emps/src/utilities directory

--- react-emps/src/utilities/ToolTip.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./ToolTip.css"; 
-
-const EmpToolTip = ({empErrMsg,classList}) => { 
-  return (<span  className={classList? classList + " empTooltip":" empTooltip"}>{" " +empErrMsg}</span>);
-}
-export default EmpToolTip;
