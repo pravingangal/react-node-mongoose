988e71a3e5c67f26ba0e732663497c3d66963dc2
Pravin Gangal <pravin.gangal@yahoo.com>
2021-10-17 21:27:50 +0530

    Update EmpLoginAction.js

--- react-emps/src/actions/EmpLoginAction.js
+++ react-emps/src/actions/EmpLoginAction.js
@@ -1,11 +1,10 @@
 import {
   EMP_LOGGED_IN,
   EMP_LOGGED_OUT,
-  EMP_LOGGED_ERR,
-  EMP_LOGGED_IN_SEND_EMAIL,
-  EMP_LOGGED_SEND_EMAIL_ERR
+  EMP_LOGGED_ERR, 
 } from "../utilities/Types";
-import { EmpApi } from "../apiemps_serverops/EmpApi";
+import EmpApi from "../apiemps_serverops/EmpApi";
+import UserSession from "../utilities/UserSession/UserSession";
 
 export const empLoggedIn = (emps) => {
   return {
@@ -14,25 +13,12 @@ export const empLoggedIn = (emps) => {
   };
 };
 
-export const empLoggedInSendEmail = (empsEmails) => {
-  return {
-    type: EMP_LOGGED_IN_SEND_EMAIL,    
-    dataLoad: empsEmails,
-  };
-};
-
-export const empLoggedOut = () => ({
+const empLoggedOut = () => ({
   type: EMP_LOGGED_OUT,
 });
 
-export const empLoggedErr = (err) => { 
+export const empLoggedErr = (err) => {
   return { type: EMP_LOGGED_ERR,   
-    errLoad: err 
-  };
-};
-
-export const empLoggedInSendEmailErr = (err) => { 
-  return { type: EMP_LOGGED_SEND_EMAIL_ERR,   
     errLoad: err 
   };
 };
@@ -41,33 +27,41 @@ export const empLoginAction = (empLoginData) => {
   return (dispatch) => {
     EmpApi.emps	
       .Login(empLoginData)
-      .then((res) => {                  
-                      localStorage.refreshJWToken = res.data.refreshToken;       
-                      dispatch(empLoggedIn(res.data));        
+      .then((res) => {        
+                      if (res.response)
+                      {                        
+                        if ("empsError" in res.response.data && +res.response.status === 400) {                          
+                          dispatch(empLoggedErr({ ...res.response.data }));
+                        }
+                                                
+                      }
+                      else
+                      {                        
+                        if (res.status === 200) {                          
+                          UserSession.setSessionVar("refreshJWToken",res.data.refreshToken,false);
+                          dispatch(empLoggedIn(res.data));
+                        }
+                        else
+                        {                         
+                          dispatch(empLoggedErr({empsError: "Network Error"}));
+                          }
+                        
+                      }
                     })
-                    .catch((e) => {
+      .catch((e) => {
+        
                           if(e.response)
                           {         
-                          dispatch(empLoggedErr(e.response.data));
-                          }        
+                                dispatch(empLoggedErr(e.response.data));
+                          }
+                          else {
+                                dispatch(empLoggedErr({empsError: "Internal Server Error"}));
+                               }
                     });
   };
 };
 
-export const empLoginSendEmailAction = (empLoginSendEmailData) => { 
-  return (dispatch) => {
-                          EmpApi.emps.loggedInSendEmail(empLoginSendEmailData).then((res) => {             
-                          dispatch(empLoggedInSendEmail(res.data));        
-      }).catch((e) => {
-        if(e.response)
-        {                 
-			      dispatch(empLoggedInSendEmailErr(e.response.data));
-        }        
-      });
-  }
-  };
-
 export const empLogoutAction = () => (dispatch) => { 
-    localStorage.removeItem("refreshJWToken");  
+  UserSession.reSetSessionVar("refreshJWToken", "");
     dispatch(empLoggedOut());
 };
