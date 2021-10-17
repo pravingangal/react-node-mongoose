aa153e437ac5289bc61cbd69b99e6e104061b646
Pravin Gangal <pravin.gangal@yahoo.com>
2021-10-17 22:10:58 +0530

    Update NotFoundPage.js

--- react-emps/src/notfound/NotFoundPage.js
+++ react-emps/src/notfound/NotFoundPage.js
@@ -4,17 +4,22 @@ import JumboTronComp from "../codesnippets/JumboTronComp";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
-const NotFoundPage = ({ empValidated }) => {
-  return ( <JumboTronComp displayMsg={"Page Not Found..!"} jumboID="notFoundPageDiv">
+const notFoundPage = (props) => {
+
+  const { empValidated } = { ...props };
+  const OTHERS = props.location.pathname.substring(1).trim(); 
+  const DISPLAY_MSG =  OTHERS=== "aboutus" || OTHERS==="contact" ? props.location.pathname.substring(1) : "Page Not Found..!";
+  
+  return ( <JumboTronComp displayMsg={DISPLAY_MSG} jumboID="notFoundPageDiv">
     <Link to={!empValidated ?'/login':'/dashboard'} className="btn btn-primary btn-lg">{!empValidated ?"Login...":"Dashboard.."}</Link>
   </JumboTronComp>                
   );
 };  
-NotFoundPage.propTypes = {  
+notFoundPage.propTypes = {  
   empValidated: PropTypes.bool.isRequired,
 };
 function mapStateToProps(state) { 
 return { empValidated: state.EmpsLoginReducer.empValidated,
 }
 }
-export default connect(mapStateToProps)(NotFoundPage);
+export default connect(mapStateToProps)(notFoundPage);
